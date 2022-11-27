02d443986b1d406d96bd6320d99b43a1843d08e9
BongQi <42308380+Blackonlearn@users.noreply.github.com>
2022-11-27 20:41:12 +0700

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -26,14 +26,6 @@ import bpy
 from bpy.props import *
 import mathutils
 
-num_bones = 2
-armature_name = 'Armature'
-ik_target_name = 'IK_Target'
-ik_pole_name = 'IK_Pole'
-fk_upperarm_name = 'FK_UpperArm'
-fk_forearm_name = 'FK_ForeArm'
-ik_forearm_name = 'IK_ForeArm'
-
 class UI(bpy.types.Panel):
     bl_label = "Snap FK/IK"
     bl_idname = "Snap_FKIK"
